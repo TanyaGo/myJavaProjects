bfdbc08bb22deb78c7c07cf5c135112eee1b581e
Tanya Shcherbyna <shcherbyna.ta@gmail.com>
2020-04-28 23:33:21 -0600

    added few methods

--- sandbox/src/main/java/javaProjects/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/javaProjects/sandbox/MyFirstProgram.java
@@ -4,25 +4,29 @@ public class MyFirstProgram {
 
   public static void main(String[] args) {
     hello("world");
-    hello ("Tanya");
+    hello("user");
+    hello("Tanya");
 
-    double len =5;
-    System.out.println("Ploschad kvadrata so storonoy" + len+ "="+ area(len));
-
+    double len = 5;
+    System.out.println("S of square with the side" + len + "=" + area(len));
 
     double a=4;
     double b=6;
-    System.out.println("Ploschad pryamoygolnika so storonami" + a + "and" + b + "=" + area(a, b));
+    System.out.println("S of rectange with sides " + a + " and " + b + " = " + area(a, b));
   }
+
   public static void hello(String somebody){
-    System.out.println("Hello,"  + somebody+ "!");
+
+    System.out.println("Hello," + somebody+ "!");
+
   }
 
   public static double area(double l){
-    return l*l;
+   return l * l;
   }
 
   public static double area(double a, double b){
-    return a*b;
+    return a * b;
+
   }
 }
